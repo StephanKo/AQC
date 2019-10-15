a45851d126d930bd2952abf484bc57db7f2e8056
StephanKo <stdmko@gmail.com>
2019-10-15 13:00:35 +0300

    Тестовый коммит 2

--- src/com/company/MyFirstApp.java
+++ src/com/company/MyFirstApp.java
@@ -2,5 +2,7 @@ public class MyFirstApp{
     public static void main (String[] args){
         System.out.println ("Наконец-то получилось!");
         System.out.println ("Всё сделал правильно!");
+        System.out.println ("Всё сделал правильно!");
+
     }
 }
