a9c356bca81a735ba153f0d192c0dd4f40dcc0b2
StephanKo <stdmko@gmail.com>
2019-10-15 12:42:54 +0300

    Тестовый коммит 1

---
+++ src/com/company/MyFirstApp.java
@@ -0,0 +1,6 @@
+public class MyFirstApp{
+    public static void main (String[] args){
+        System.out.println ("Наконец-то получилось!");
+        System.out.println ("Всё сделал правильно!");
+    }
+}
